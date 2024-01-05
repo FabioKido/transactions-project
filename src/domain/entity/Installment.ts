4baa326f1fa9cd7b232fabcea33fe8f9ee498bba
FabioKido <fabiohenryquemesquita@gmail.com>
2024-01-05 17:48:59 -0300

    feat(installment): created entity

---
+++ src/domain/entity/Installment.ts
@@ -0,0 +1,11 @@
+/**
+ * Camada 1
+ * Entity
+ * A entity representa uma regra de negócio e tem uma identificação única
+ */
+
+export default class Installment {
+
+    constructor(readonly number: number, readonly amount: number) {}
+
+}
