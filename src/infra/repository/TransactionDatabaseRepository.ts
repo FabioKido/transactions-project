b758c1bab508a24c108b67d47761731898ae7a67
FabioKido <fabiohenryquemesquita@gmail.com>
2024-01-05 18:51:48 -0300

    feat(repository/adapters): updated database

--- src/infra/repository/TransactionDatabaseRepository.ts
+++ src/infra/repository/TransactionDatabaseRepository.ts
@@ -5,6 +5,7 @@
  * Cria-se(nessa arquitetura) um Contrato(Interface) e uma Implementação(Classe)
  */
 
+import Installment from "../../domain/entity/Installment";
 import Transaction from "../../domain/entity/Transaction";
 import TransactionRepository from "../../domain/repository/TransactionRepository";
 import Connection from "../database/Connection";
@@ -14,10 +15,20 @@ export default class TransactionDatabaseRepository implements TransactionReposit
     constructor(readonly connection: Connection) {}
     
     async save(transaction: Transaction): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.connection.query("insert into public.transaction (code, amount, number_installments, payment_method) values ($1, $2, $3, $4)", [transaction.code, transaction.amount, transaction.numberInstallments, transaction.paymentMethod]);
+		for (const installment of transaction.installments) {
+			await this.connection.query("insert into public.installment (code, number, amount) values ($1, $2, $3)", [transaction.code, installment.number, installment.amount]);
+		}
     }
 
     async get(code: string): Promise<Transaction> {
-        throw new Error("Method not implemented.");
+        const transactionData = await this.connection.findOne("select * from public.transaction where code = $1", [code]);
+		const transaction = new Transaction(transactionData.code, parseFloat(transactionData.amount), transactionData.number_installments, transactionData.payment_method);
+		const installmentsData = await this.connection.query("select * from public.installment where code = $1", [code]);
+		for (const installmentData of installmentsData) {
+			const installment = new Installment(installmentData.number, parseFloat(installmentData.amount));
+			transaction.installments.push(installment);
+		}
+		return transaction;
     }
 }
